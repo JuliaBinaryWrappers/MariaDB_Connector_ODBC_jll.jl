db166b935ae1dad2cb133e4bbfe22938b69b83e6
jlbuild <juliabuildbot@gmail.com>
2026-01-29 18:02:36 +0000

    MariaDB_Connector_ODBC_jll build 3.1.22+0

--- src/MariaDB_Connector_ODBC_jll.jl
+++ src/MariaDB_Connector_ODBC_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MariaDB_Connector_ODBC")
-JLLWrappers.@generate_main_file("MariaDB_Connector_ODBC", UUID("1124d457-72a3-5205-9757-36a127c7d0f8"))
+JLLWrappers.@generate_main_file("MariaDB_Connector_ODBC", Base.UUID("1124d457-72a3-5205-9757-36a127c7d0f8"))
 end  # module MariaDB_Connector_ODBC_jll
